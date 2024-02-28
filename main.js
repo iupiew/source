0db02b2cb787c617b2100cf180e59d58759b5453
iupiew <iupiew666@gmail.com>
2024-02-28 08:02:17 -0500

    add links

--- main.js
+++ main.js
@@ -3,16 +3,22 @@ import lightModeIcon from '/images/light_mode.svg'
 import darkModeIcon from '/images/dark_mode.svg'
 
 document.querySelector('#app').innerHTML = `
+<div id="content">
+  <h2 id="url">https://iupiew.xyz</h2>
 
-  <div id="content">
-    <h2 id="url">https://iupiew.xyz</h2>
-
-    <div class="dark-mode-buttons">
-      <button class="dark-mode-button" id="dark-mode-on"><img src="${darkModeIcon}" width="24" height="24" alt="Dark mode" aria-label="dark mode toggle" title="Dark mode"></button>
-      <button class="dark-mode-button" id="dark-mode-off"><img src="${lightModeIcon}" width="24" height="24" alt="Light mode" aria-label="light mode toggle" title="Light mode"></button>
-    </div>
-    
-    <h2 id="intro-text"">Hi! This is my website! Currently there is nothing here...</h2>
+  <div class="dark-mode-buttons">
+    <button class="dark-mode-button" id="dark-mode-on"><img src="${darkModeIcon}" width="24" height="24" alt="Dark mode" aria-label="dark mode toggle" title="Dark mode"></button>
+    <button class="dark-mode-button" id="dark-mode-off"><img src="${lightModeIcon}" width="24" height="24" alt="Light mode" aria-label="light mode toggle" title="Light mode"></button>
+  </div>
+  
+  <h2 id="intro-text"">Hi! This is my website! Currently there is nothing here...</h2>
+  
+  <div id="links">
+    <a id="link" href="www.linkedin.com/in/vasiliyo">Linkedin</a>
+    <a id="link" href="https://github.com/iupiew">Github</a>
+    <a id="link" href="https://github.com/iupiew/source">Source code</a>
+  </div>
+<div> 
 `
 const cls = document.body.classList;
   const getSessionTheme = sessionStorage.getItem("theme");
